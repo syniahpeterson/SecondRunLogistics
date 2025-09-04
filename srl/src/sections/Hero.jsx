1ea651604142bd70377315c3cdb31fc0eb70d774
syniahpeterson <syniahpeterson@gmail.com>
2025-09-04 16:39:17 -0400

    Add main placeholder components and styles for the application

---
+++ srl/src/sections/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Hero.css";
+
+const Home = () => {
+  return (
+    <section id="home">
+      <h2>Hero Section Content</h2>
+    </section>
+  );
+};
+
+export default Home;
